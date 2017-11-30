821ebeb810f37eb1a753de732550146aca982225
Alessandro Solimando <asolimando@users.noreply.github.com>
2017-11-30 19:52:51 +0100

    full refactoring for increasing generics design, parallelize the test

--- src/test/scala/HIPSTester.scala
+++ src/test/scala/HIPSTester.scala
@@ -2,17 +2,20 @@ import org.scalatest.FunSuite
 
 import com.github.asolimando.hips._
 
+import java.util.concurrent.atomic._
 import java.util.Random
 
 class HIPSTester extends FunSuite {
   test("Bruteforce check computed solutions") {
 
     val rnd = new Random(2)
+    val c = new AtomicInteger(0)
 
-    for(i <- 1 to 100000){
-      if(i % 10000 == 0) println(i)
+    (1 to 100000).par.foreach{i =>
+      val j = c.getAndIncrement
+      if(j % 10000 == 0) println(j)
       val points = (1 to 15).map(_ => HIPS.generatePoint(100, 100, 10.0, rnd))
-      HIPS.exec(points)
+      HIPS.hipsPnt.exec(points, true)
     }
   }
 }
