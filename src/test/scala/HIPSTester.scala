6c23681a284362c781a9342eb260e58c4d21d913
Alessandro Solimando <18898964+asolimando@users.noreply.github.com>
2017-12-07 23:36:32 +0100

    scaladoc added, refactoring for class subdivision, type genericity to be improved to abstract from Pnt

---
+++ src/test/scala/HIPSTester.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FunSuite
+
+import org.asolimando.hips._
+import org.asolimando.point.pntpackage.Pnt
+
+class HIPSTester extends FunSuite {
+  test("Bruteforce check computed solutions") {
+      for(i <- 1 to 100000){
+        if(i % 10000 == 0) println(i)
+        val points = (1 to 15).map(_ => HIPS.generatePoint(100,100,10.0))
+        HIPS.exec(points)
+      }
+  }
+}
